22bb01935c75448a01260a4d1940f3a450a884ef
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-03-12 13:42:30 +0530

    changes

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket ="devopsbucket" # Replace with your actual S3 bucket name
+    bucket ="jadhav-devops" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
